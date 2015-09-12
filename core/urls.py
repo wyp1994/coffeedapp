1da40cd854e4b4f4cd596e6cfe42b6d5248c720f
Casber <casberw@gmail.com>
2015-09-12 02:08:44 -0700

    Basic blog

--- core/urls.py
+++ core/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import include, url
 import core.views as coreviews
 
 urlpatterns = [
-    url(r'^$', coreviews.LandingView.as_view()),   
+    url(r'^$', coreviews.LandingView.as_view()),
+    url(r'^location/$', coreviews.LocationListView.as_view()),
+    url(r'^location/(?P<pk>\d+)/detail/$', coreviews.LocationDetailView.as_view(), name='location_list'),   
 ]
